645263956bbfd683f5001b4134a99146569f4aa4
bertrandvidal <vidal.bertrand@gmail.com>
2022-05-02 10:28:17 -0400

    Blender script to generate random linked cubes
    A quick and dirty tests to generate cubes in Blender that are all linked
    and subsequently merged into one 3d shape.
    
    Note that the "sculpture" can be exported to a .stl format which in turn
    can be import in a slicer software making it 3d printable :+1:
    
    Tests:
    - see above about stl export > slicer

---
+++ ant.py
@@ -0,0 +1,49 @@
+import bpy
+from random import randint, shuffle, choice
+from time import sleep
+
+[bpy.data.objects.remove(o) for o in bpy.context.scene.objects]
+
+location = (0, 0, 0)
+cubes = {location}
+bpy.ops.mesh.primitive_cube_add(location=location)
+
+transformations = [
+ lambda x, y, z: (x, y+2, z),
+ lambda x, y, z: (x, y+2, z),
+ lambda x, y, z: (x, y-2, z),
+ lambda x, y, z: (x+2, y, z),
+ lambda x, y, z: (x-2, y, z),
+ lambda x, y, z: (x, y, z+2),
+ lambda x, y, z: (x, y, z-2),
+]
+
+
+def get_neighboring_location(location, cubes):
+    new_location = None
+
+    while not new_location:
+        shuffle(transformations)
+        for transformation in transformations:
+            potential_location = transformation(*location)
+            if potential_location not in cubes:
+                new_location = potential_location
+                break
+            return get_neighboring_location(choice(list(cubes)), cubes)
+
+
+    cubes.add(new_location)
+    return new_location
+
+
+for _ in range(500):
+    location = get_neighboring_location(location, cubes)
+    bpy.ops.mesh.primitive_cube_add(location=location)
+
+
+c = {}
+c["object"] = c["active_object"] = bpy.context.object
+c["selected_objects"] = c["selected_editable_objects"] = bpy.context.scene.objects
+bpy.ops.object.join(c)
+
+raise Exception("Buuuuuuuuurt")
